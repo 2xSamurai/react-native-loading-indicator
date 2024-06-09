0dc26a98ea9c8b050b79a16d7e7e60a5b6d8cec5
Deepak Vijayan <deepak@awsm.in>
2024-06-09 16:19:59 +0530

    UPDATE: square plane flip animation

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-import React from "react";
-import { StyleSheet, ActivityIndicator, Dimensions } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, Dimensions, Animated, Easing } from "react-native";
 
-const LoadingIndicator = ({ loading = false, style = {} }) => {
+const LoadingIndicator = ({
+	loading = false,
+	indicatorStyles = {},
+	duration = 1200,
+	bezierFn = {
+		x1: 0.37,
+		y1: 0,
+		x2: 0.63,
+		y2: 1,
+	},
+	iterations = -1,
+	callback = () => {},
+}) => {
+	const progress = useRef(new Animated.Value(0)).current;
+
+	useEffect(() => {
+		progress.setValue(0);
+		startAnimation();
+	}, [loading, indicatorStyles, duration, bezierFn, iterations, callback]);
+
+	const startAnimation = () => {
+		Animated.timing(progress, {
+			toValue: 1,
+			duration: duration,
+			useNativeDriver: true,
+			easing: Easing.inOut(
+				Easing.bezier(
+					bezierFn?.x1,
+					bezierFn?.y1,
+					bezierFn?.x2,
+					bezierFn?.y2
+				)
+			),
+			iterations: iterations,
+		}).start(callback);
+	};
+	const rotateX = progress.interpolate({
+		inputRange: [0, 0.5, 1],
+		outputRange: ["0deg", "-180deg", "-180deg"],
+		extrapolate: "clamp",
+	});
+
+	const rotateY = progress.interpolate({
+		inputRange: [0, 0.5, 1],
+		outputRange: ["0deg", "0deg", "-180deg"],
+		extrapolate: "clamp",
+	});
+
 	return loading ? (
-		<ActivityIndicator
-			width={100}
-			style={[styles.indicator, style]}
-			color={"white"}
-		/>
+		<Animated.View
+			style={[
+				styles.indicator,
+				{
+					transform: [
+						{ perspective: 120 },
+						{ rotateX: rotateX },
+						{ rotateY: rotateY },
+					],
+				},
+				indicatorStyles,
+			]}
+		></Animated.View>
 	) : null;
 };
 
@@ -16,10 +71,11 @@ export default LoadingIndicator;
 const styles = StyleSheet.create({
 	indicator: {
 		position: "absolute",
-		left: Dimensions.get("window").width / 2 - 10,
-		top: Dimensions.get("window").height / 2 - 10,
+		left: Dimensions.get("window").width / 2 - 20,
+		top: Dimensions.get("window").height / 2 - 20,
 		zIndex: 10,
-		width: 20,
-		height: 20,
+		width: 40,
+		height: 40,
+		backgroundColor: "white",
 	},
 });
